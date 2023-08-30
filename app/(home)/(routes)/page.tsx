f97d104e7fe3fd547ab93b49711c3259c202cce1
Michael Yeates <michael-yeates@outlook.com>
2023-08-30 21:15:03 +0100

    Add user auth with clerk/next.js

--- app/(home)/(routes)/page.tsx
+++ app/(home)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div className="">
-      <p>This is a protected route</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
